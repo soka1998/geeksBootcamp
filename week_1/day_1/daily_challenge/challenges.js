c77f5fb9876a3564fda930b41d109003dabd5d18
soka1998 <soukainaharifi18@gmail.com>
2026-01-27 14:04:08 +0000

    add stars challenges

--- week_1/day_1/daily_challenge/challenges.js
+++ week_1/day_1/daily_challenge/challenges.js
@@ -1,4 +1,28 @@
-// ====== Daily Challenge 1
+// ====== Daily Challenge 1 : stars
+
+let stars = "";// This variable will store the stars we build step by step
+for (let i = 1; i <= 6; i++) {
+    // Add one star to the string in each iteration
+    stars = stars + "*";
+    console.log(stars);
+}
+//nested loop 
+//for lines 6
+for (let i = 1; i <= 6; i++) {
+    let stars = "";
+    //for stars in each line
+    for (let j = 1; j <= i; j++) {
+
+        // Inner loop controls the number of stars in each row
+
+        stars = stars + "*";
+    }
+    console.log(stars);
+}
+
+
+
+
 
 // ====== Daily Challenge 2
 
