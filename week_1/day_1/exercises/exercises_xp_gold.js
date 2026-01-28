5f19f2e734e2d0dc20c7ca16b3f4ed24e5f9b7fa
soka1998 <soukainaharifi18@gmail.com>
2026-01-28 09:14:27 +0000

    Add attendance challenge

--- week_1/day_1/exercises/exercises_xp_gold.js
+++ week_1/day_1/exercises/exercises_xp_gold.js
@@ -1,3 +1,6 @@
+
+
+
 // ===== Exercise 1 :Divisible by three
 let numbers = [123, 8409, 100053, 333333333, 7]
 for (let i = 0; i < numbers.length; i++) {
@@ -12,9 +15,42 @@ for (let i = 0; i < numbers.length; i++) {
     }
 
 }
-// ===== Exercise 2
+// ===== Exercise 2 : Attendance
+let guestList = {
+    randy: "Germany",
+    karla: "France",
+    wendy: "Japan",
+    norman: "England",
+    sam: "Argentina"
+}
+//let student = prompt("Enter your name:");
+// Convert the input to lowercase so it matches the keys in guestList
+//student = student.toLowerCase();
 
-// ===== Exercise 3
+// Check if the entered name exists as a key in the guestList object
+//if (student in guestList) {
+// guestList[student] retrieves the value (country) associated with that name
+//  console.log("Hi! I'm " + student + ", and I'm from " + guestList[student] + ".");
+//} else {
+// console.log("Hi,I'm  guest.");
+//}
+
+// ===== Exercise 3: playing with numbers 
+let age = [20, 5, 12, 43, 98, 55];
+let sum = 0;
+for (let i = 0; i < age.length; i++) {
+    sum += age[i];
+}
+console.log("The sum of ages is: " + sum);
+//Highest age
+let maxAge = age[0]; // Start with the first age as maximum
+for (let i = 1; i < age.length; i++) {// Start from second element
+    if (age[i] > maxAge) {// Compare current element with max
+        maxAge = age[i]; // Update max if current is bigger
+    }
+}
+console.log("Highest age:", maxAge);
+
 
 // ===== Exercise 4
 
