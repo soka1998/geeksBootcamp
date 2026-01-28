cc731c7f9f6820bbba749a28ca406cb544a9d121
soka1998 <soukainaharifi18@gmail.com>
2026-01-28 08:18:37 +0000

    add 'NOT BAD' challenge

--- week_1/day_1/daily_challenge/challenges.js
+++ week_1/day_1/daily_challenge/challenges.js
@@ -13,7 +13,7 @@ for (let i = 1; i <= 6; i++) {
     //for stars in each line
     for (let j = 1; j <= i; j++) {
 
-        // Inner loop controls the number of stars in each row
+        // Inner loop controls the number of stars in each line
 
         stars = stars + "*";
     }
@@ -21,9 +21,21 @@ for (let i = 1; i <= 6; i++) {
 }
 
 
+// ====== Daily Challenge 2: NOT BAD
 
+let sentence = "The movie is not that bad, I like it";
 
-
-// ====== Daily Challenge 2
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+//check if "not and "bad" are in the sentence and "bad" follows "not" exists and if "bad" comes after "not"
+if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+    // "!==" means strict not equal (checks both value and type)
+    let newSentence = sentence.slice(0, wordNot)
+        + "good"
+        + sentence.slice(wordBad + 3);
+    console.log(newSentence);
+} else {
+    console.log(sentence);
+}
 
 // ====== Daily Challenge 3
