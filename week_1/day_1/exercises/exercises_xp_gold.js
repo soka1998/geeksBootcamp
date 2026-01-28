027dbd6294e61aba319b253aaa005f9459e01b61
soka1998 <soukainaharifi18@gmail.com>
2026-01-28 08:32:13 +0000

    Add 'divisible by three' challenge

--- week_1/day_1/exercises/exercises_xp_gold.js
+++ week_1/day_1/exercises/exercises_xp_gold.js
@@ -1,4 +1,17 @@
-// ===== Exercise 1
+// ===== Exercise 1 :Divisible by three
+let numbers = [123, 8409, 100053, 333333333, 7]
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 3 === 0) {
+        // If the remainder is 0, the number is divisible by 3
+        console.log(true)
+        //console.log(numbers[i] + " is divisible by three"); 
+
+    } else {
+        console.log(false)
+        // console.log(numbers[i] + " is not divisible by three");
+    }
+
+}
 // ===== Exercise 2
 
 // ===== Exercise 3
